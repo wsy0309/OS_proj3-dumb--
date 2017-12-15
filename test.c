b7bf2064f2af64f0000963eb03fbd7c401b15600
wsy0309 <wsy0309@naver.com>
2017-12-15 23:31:08 +0900

    ing

---
+++ test.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <fcntl.h> 
+#include <unistd.h>   
+#include "fs.h"
+#include "procqADT.h"
+
+partition* part;
+Pcb* pcb;
+
+void fileMount(){
+	
+	int fd = open("disk.img",O_RDONLY);
+            
+    printf("superblock\n");
+    part = (partition*) malloc(sizeof(partition));;
+    read(fd,&part->s,sizeof(super_block));
+    printf("%x\n",part->s.first_data_block);      
+
+    int i;
+    printf("inode!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+    for(i=0;i<224;i++){
+        read(fd,&(part->inode_table[i]),sizeof(inode));
+        for(int j = 0;j<6;j++){
+			printf("%x\t",part->inode_table[i].blocks[j]);
+		}
+		printf("\n");
+    }   
+            
+    printf("datablock!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+    for(i=0;i<4088;i++){
+            read(fd,&part->data_blocks[i],sizeof(blocks));   
+            //printf("%s\n",part->data_blocks[i].d);
+    }   
+    close(fd);
+}
+void fileOpen(char* filename,unsigned int mode){
+
+	inode* root = (inode*) malloc(sizeof(inode));
+	root = &part->inode_table[part->s.first_inode];
+	printf("root Dir inode : %x\n",part->s.first_inode);
+	printf("root Dir size : %x\n",root->size);
+
+	dentry* entry = (dentry*) malloc(sizeof(dentry));
+	entry = &part->data_blocks[root->blocks[0]];
+	printf("entry[0] : %s\n",entry);
+
+	printf("dentry length : %d\n",entry->dir_length);
+	
+	while(1){
+		if (strcmp(filename, entry->name) == 0){
+			pcb->desc = (Descriptor*) malloc(sizeof(Descriptor));
+			pcb->desc->file_name = filename;
+			pcb->desc->mode = mode;
+			pcb->desc->cur_node = &part->inode_table[entry->inode];
+
+			printf("entry inode : %x, entry file_name : %s, entry mode : %d\n",entry->inode,pcb->desc->file_name,pcb->desc->mode);
+			break;
+		}
+		else{
+			entry = (char*)entry + entry->dir_length;
+			printf("entry : %s\n",entry);
+			printf("entry file name : %s\n",entry->name);
+		}
+
+	}
+}
+
+void fileRead(unsigned int buf_size){
+	if(pcb->desc->mode == 32151679){
+		unsigned short block = pcb->desc->cur_node->blocks[0];
+		printf("date : %s\n", part->data_blocks[block].d);
+		printf("date size : %d\n", sizeof(part->data_blocks[block].d));
+	}
+}
+
+int main(){
+	pcb = (Pcb*) malloc(sizeof(Pcb));
+	fileMount();
+	fileOpen("file_1",32151679);
+
+	printf("pcb desc : %s, mode : %d, inode->block : %d \n", pcb->desc->file_name, pcb->desc->mode, pcb->desc->cur_node->blocks[0]);
+	
+	fileRead(1024);
+
+	return;
+}
+
+
